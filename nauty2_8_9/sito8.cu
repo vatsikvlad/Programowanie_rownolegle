822a22b60f9b47b2840b5238936a2845f04f2b41
Vladyslav Vatslavyi <vatsikvlad@gmail.com>
2026-05-25 12:54:24 +0200

    Refactor code and changed float to double

--- nauty2_8_9/sito8.cu
+++ nauty2_8_9/sito8.cu
@@ -10,7 +10,6 @@
 #define BATCH_SIZE 65536
  
 __global__ void test(char *d_bus, int *d_results, int total_graphs) {
-
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx >= total_graphs) return;
 
@@ -19,12 +18,12 @@ __global__ void test(char *d_bus, int *d_results, int total_graphs) {
         buffer[i] = d_bus[idx * GLEN + i];
     }
  
-    int i,j,k,k3,k4,L,L1,z;
-    float eps,g,h,ma,mn,norm,s,t,u,w;
+    int i, j, k, k3, k4, L, L1, z;
+    double eps, g, h, ma, mn, norm, s, t, u, w;
     int cond;
-    float d[NMAX+1], e[NMAX+1], e2[NMAX+1], Lb[NMAX+1];
-    float x[NMAX+1];
-    float a[NMAX*(NMAX-1)/2 + NMAX + 1];
+    double d[NMAX + 1], e[NMAX + 1], e2[NMAX + 1], Lb[NMAX + 1];
+    double x[NMAX + 1];
+    double a[NMAX * (NMAX - 1) / 2 + NMAX + 1];
     int n;
     int bit, poz, poz2;
     bit = 32;
@@ -35,100 +34,203 @@ __global__ void test(char *d_bus, int *d_results, int total_graphs) {
 
     n = BUFFOR[0] - 63;
     a[0] = 0.0;
-    for (i = 0; i < n; i++)
-    for (j = 0; j<=i; j++)
-    {
-        if (i==j) {a[poz2++] = 0; }
-        else {
-            if (bit == 0) { bit = 32;  poz++; }
-            if ((BUFFOR[poz] - 63) & bit)
-                    { a[poz2++] = 1; }
-                else
-                    { a[poz2++] = 0; }
-            bit = bit >> 1;
+    for (i = 0; i < n; i++){
+        for (j = 0; j <= i; j++)
+        {
+            if (i == j) {
+                a[poz2++] = 0; 
+            }
+            else {
+                if (bit == 0) { 
+                    bit = 32;  
+                    poz++; 
+                }
+                if ((BUFFOR[poz] - 63) & bit){ 
+                    a[poz2++] = 1; 
+                }
+                else{ 
+                    a[poz2++] = 0; 
+                }
+                bit = bit >> 1;
+            }
         }
     }
     
     int k1 = 1;
     int k2 = n;
+    i = 0;
+    for (L = 1; L <= n; L++) { 
+        i += L; 
+        d[L] = a[i]; 
+    }
+
+    for (L = n; L >= 2; L--)
     {
-        i = 0;
-        for (L=1;L<=n;L++) { i += L; d[L] = a[i]; }
-    
-        for (L=n;L>=2;L--)
-        {
-        i--; j = i; h = a[j]; s = 0;
-        for (k=L-2;k>=1;k--) { i--; g = a[i]; s += g*g; }
+        i--; 
+        j = i; 
+        h = a[j]; 
+        s = 0;
+        for (k = L - 2; k >= 1; k--) { 
+            i--; 
+            g = a[i]; 
+            s += g * g; 
+        }
         i--;
-        if (s == 0) { e[L] = h; e2[L] = h*h; a[j] = 0.0; }
-        else
-            {
-            s += h*h; e2[L] = s; g = sqrtf(s); if (h>=0.0) g=-g;
+        if (s == 0) {
+            e[L] = h; 
+            e2[L] = h * h; 
+            a[j] = 0.0; 
+        }
+        else {
+            s += h * h; 
+            e2[L] = s; 
+            g = sqrtf(s); 
+            if (h >= 0.0) {
+                g=-g;
+            }
             e[L] = g;
-            s = 1.0 / (s-h*g);
-            a[j] = h - g; h = 0.0; L1 = L - 1; k3 = 1;
-            for (j=1;j<=L1;j++)
+            s = 1.0 / (s - h * g);
+            a[j] = h - g; 
+            h = 0.0; 
+            L1 = L - 1; 
+            k3 = 1;
+            for (j = 1; j <= L1; j++)
             {
                 k4 = k3; g = 0;
-                for (k=1;k<=L1;k++) { g +=a[k4]*a[i+k]; 
-                                    if (k<j)  z = 1; else z = k;
-                                    k4 += z; }
-                k3 += j; g *= s; e[j] = g; h += a[i+j]*g;
+                for (k = 1; k <= L1; k++) { 
+                    g += a[k4] * a[i + k]; 
+                    if (k < j){
+                        z = 1;
+                    }
+                    else{
+                        z = k;
+                    }
+                    k4 += z; 
+                }
+                k3 += j; 
+                g *= s; 
+                e[j] = g; 
+                h += a[i + j] * g;
             }
-            h *= 0.5*s; k3 = 1;
-            for (j=1;j<=L1;j++)
+            h *= 0.5 * s; 
+            k3 = 1;
+            for (j = 1; j <= L1; j++)
             {
-                s = a[i+j]; g = e[j]-h*s; e[j] = g;
-                for (k=1;k<=j;k++) { a[k3] += -s*e[k]-a[i+k]*g; k3++; }
+                s = a[i + j]; 
+                g = e[j] - h * s; 
+                e[j] = g;
+                for (k = 1; k <= j; k++) { 
+                    a[k3] += -s * e[k] - a[i + k] * g; 
+                    k3++; 
+                }
             }
-            }
-        h = d[L]; d[L] = a[i+L]; a[i+L] = h;
         }
-        h = d[1]; d[1] = a[1]; a[1] = h; e[1] = 0.0; e2[1] = 0.0; s = d[n];
-        t = fabsf(e[n]); mn = s - t; ma = s + t;
-        for (i=n-1;i>=1;i--)
-        {
-        u = fabsf(e[i]); h = t + u; t = u; s = d[i]; u = s - h;
-        if (u < mn) mn = u;
+        h = d[L]; 
+        d[L] = a[i + L]; 
+        a[i + L] = h;
+    }
+    h = d[1]; 
+    d[1] = a[1]; 
+    a[1] = h; 
+    e[1] = 0.0; 
+    e2[1] = 0.0; 
+    s = d[n];
+    t = fabsf(e[n]); 
+    mn = s - t; 
+    ma = s + t;
+    for (i = n - 1; i >= 1; i--)
+    {
+        u = fabsf(e[i]); 
+        h = t + u; 
+        t = u; 
+        s = d[i]; 
+        u = s - h;
+        if (u < mn) {
+            mn = u;
+        }
         u = s + h;
-        if (u > ma) ma = u;
+        if (u > ma) {
+            ma = u;
         }
-        for (i=1;i<=n;i++) { Lb[i] = mn; x[i] = ma; }
-        norm = fabsf(mn); s = fabsf(ma);
-        if (s>norm) norm = s;
-        w = ma; eps = 7.28e-17f*norm;
-        for (k=k2;k>=k1;k--)
-        {
-         s = mn; i = k;
-        do {cond = 0; g = Lb[i];
-            if (s < g) s = g; else { i--; if (i>=k1) cond = 1; }
+    }
+    for (i = 1; i <= n; i++) { 
+        Lb[i] = mn; 
+        x[i] = ma; 
+    }
+    norm = fabsf(mn); 
+    s = fabsf(ma);
+    if (s > norm) {
+        norm = s;
+    }
+    w = ma; 
+    eps = 7.28e-17f * norm;
+    for (k = k2; k >= k1; k--)
+    {
+        s = mn; 
+        i = k;
+        do {
+            cond = 0; 
+            g = Lb[i];
+            if (s < g) {
+                s = g;
+            } 
+            else { 
+                i--; 
+                if (i >= k1) {
+                    cond = 1;
+                } 
+            }
         } while (cond);
         g = x[k];
-        if (w>g) w = g;
-        while (w-s>2.91e-16*(fabsf(s)+fabsf(w))+eps)
+        if (w > g) {
+            w = g;
+        }
+        while (w - s > 2.91e-16 * (fabsf(s) + fabsf(w)) + eps)
         {
-            if (floorf(w+10e-5)<s-10e-5) return;  // przedział nie zawiera liczby całkowitej
-            L1 = 0; g = 1.0; t = 0.5*(s+w);
-            for (i=1;i<=n;i++)
+            if (floorf(w + 10e-5) < s - 10e-5) {
+                return;
+            }  // przedział nie zawiera liczby całkowitej
+            L1 = 0; 
+            g = 1.0; 
+            t = 0.5 * (s + w);
+            for (i = 1; i <= n; i++)
             {
-                if (g!=0)  g = e2[i] / g; else g = fabsf(6.87e15*e[i]);
-                g = d[i]-t-g;
-                if (g<0) L1++;
-            }
-            if (L1<k1) { s = t; Lb[k1] = s; }
-            else
-            { if (L1<k)
-                {
-                    s = t; Lb[L1+1] = s;
-                    if (x[L1]>t) x[L1] = t;
+                if (g != 0) {
+                    g = e2[i] / g;
+                } 
+                else {
+                    g = fabsf(6.87e15 * e[i]);
                 }
-                else w = t;
+                g = d[i] - t - g;
+                if (g < 0) {
+                    L1++;
+                }
+            }
+            if (L1 < k1) { 
+                s = t; 
+                Lb[k1] = s; 
+            }
+            else
+            { 
+                if (L1 < k)
+                {
+                    s = t; 
+                    Lb[L1 + 1] = s;
+                    if (x[L1] > t) {
+                        x[L1] = t;
+                    }
+                }
+                else {
+                    w = t;
+                }
             }
         }
-        u = 0.5*(s+w); x[k] = u;
-        if  (!(( ceilf(u) - u  < 10e-5 ) || ( u - floorf(u) < 10e-5 ))) { return; };
+        u = 0.5 * (s + w); 
+        x[k] = u;
+        if (!((ceilf(u) - u < 10e-5) || (u - floorf(u) < 10e-5))) { 
+            return; 
         }
-    }   
+    }
     d_results[idx] = 1;
 }
 
